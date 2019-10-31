23e4ea27cd029e7b0624e97a7da823191f07e91b
biareichert <beatrizreichert99@gmail.com>
2019-10-31 11:09:53 -0300

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -24,8 +24,6 @@ public class ChatServer{
 
   private static final String ACCESS_TOKEN = "bV9CF_mwE-AAAAAAAAABTyllMAsCYrSID0reQ-u5pI2U375noLTnfGhTov48M04b";
   private static final String dropboxDir = "dropbox_chat_bia", clienteEntrada="cliente", clienteSaida = "cliente", server_dir="arquivos_server", extensaoClientEntrada=".client", extensaoClientSaida=".chat", extensaoServer=".serv";
-  //server_dir é o nome do diretorio do servidor local e dropbox (mesmo nome)
-
   public static DbxRequestConfig config;
   public static DbxClientV2 client;
   public static FullAccount account;
@@ -68,14 +66,13 @@ public class ChatServer{
   public void baixarArquivoDropbox(String arquivo, String destino, String extensao) throws DbxException, IOException{
       DbxDownloader<FileMetadata> downloader = client.files().download(arquivo);
       try {
-          System.out.println("baixando: "+arquivo);
-
-          String [] div=arquivo.split("/");//arquivo = /dropbox../f_saida_../nomeArquivo.extensao::: extrair o arquivo
+        //  System.out.println("baixando: "+arquivo);
+          String [] div=arquivo.split("/");
 
           String nomeArquivo = div[div.length-1];
           nomeArquivo=nomeArquivo.replace(extensaoClientSaida, extensao);
 
-          FileOutputStream out = new FileOutputStream(destino+"/"+nomeArquivo);///////////////aqui alterar a extensao .serv para download no server
+          FileOutputStream out = new FileOutputStream(destino+"/"+nomeArquivo);
           downloader.download(out);
           out.close();
       } catch (DbxException ex) {
@@ -85,10 +82,9 @@ public class ChatServer{
 
   public void uploadArquivoDropbox(String arquivo, String nomeCliente, String destino, String extensao, String nome) throws DbxException, IOException{
       try {
-          String [] div=arquivo.split("/");//arquivo = /dropbox../f_saida_../nomeArquivo.extensao::: extrair o arquivo
+          String [] div=arquivo.split("/");
           String nomeArquivo = div[div.length-1];
           String conteudoArq = new String(Files.readAllBytes(Paths.get(nomeArquivo)));
-          System.out.println(conteudoArq);
           String novoNome = "";
           if(extensao.equals(".client")){
             novoNome = nome + "-" + nomeCliente + ".client";
@@ -97,10 +93,6 @@ public class ChatServer{
               novoNome = nomeCliente+extensaoServer;
             }
           }
-
-          System.out.println("novoNome: "+novoNome);
-          System.out.println("extensao: "+extensao);
-          System.out.println("destino: "+destino);
 
           try	{
   			       FileWriter arq = new FileWriter(server_dir+"/"+novoNome);
@@ -111,9 +103,8 @@ public class ChatServer{
   			       ex.printStackTrace();
   		   }
 
-          //nomeArquivo=nomeArquivo.replace(extensaoClientSaida, extensaoServer);
-          System.out.println("upload: "+server_dir+"/"+novoNome+" :: destino: "+dropboxDir+"/"+novoNome);
-          // if(extensao.compareTo(extensaoClientEntrada)!=0){
+          System.out.println("Fazendo Upload...");
+
           InputStream in = new FileInputStream(server_dir+"/"+novoNome);
           FileMetadata metadata = client.files().uploadBuilder("/"+dropboxDir+"/"+novoNome).uploadAndFinish(in);
       }catch (DbxException ex) {
@@ -133,52 +124,45 @@ public class ChatServer{
       ChatServer cs = new ChatServer();
 
       while(true){
-      //  System.out.println("Verificando atualizacoes no servidor....Nao fechar!");
+        System.out.println("Verificando Arquivos...");
         List<String> diretoriosDropbox = cs.listarDiretoriosDropboxChat_clientes();
         List<String> clientes = new ArrayList<String>();
 
-        //encontrar os clientes que ja acessaram o dropbox
         for(String dir : diretoriosDropbox){
           if(dir.contains(clienteEntrada)){
-            clientes.add(dir.split(clienteEntrada+"_")[1]);// /f_entrada_nomeCliente
+            clientes.add(dir.split(clienteEntrada+"_")[1]);
           }
         }
-        //criar o diretorio fisico em server_dir
-        //String nomeCli;
 
         for(String cl : clientes){
           String teste = server_dir+"/"+cl;
-        //  System.out.println(teste);
           File diretorio = new File(server_dir+"/"+cl);
           if(!diretorio.exists()){
             diretorio.mkdir();
           }
 
-          List<String> clienteArquivos = cs.listarArquivosDiretorioDropbox("/"+dropboxDir+"/"+clienteSaida+"_"+cl);//retorna o caminho completo de cada arquivo
+          List<String> clienteArquivos = cs.listarArquivosDiretorioDropbox("/"+dropboxDir+"/"+clienteSaida+"_"+cl);
 
           for(String ca : clienteArquivos){
-            //cs.baixarArquivoDropbox(ca,server_dir+"/"+cl,extensaoServer);
             cs.deletarArquivoDropbox(ca);
 
             for(String cl1 : clientes){
-
-              if(cl1.compareTo(cl)==0){//nao enviar para o mesmo cliente de que se foi baixado
+              if(cl1.compareTo(cl)==0){
                 cs.uploadArquivoDropbox(ca,cl,"/"+dropboxDir+"/"+server_dir+"/"+cl, extensaoServer, cl);
                 continue;
               }
-
               cs.uploadArquivoDropbox(ca,cl,"/"+dropboxDir+"/"+clienteEntrada+"_"+cl1, extensaoClientEntrada, cl1);
             }
-
           }
-
         }
 
-        System.out.println("Tudo pronto!");
+        System.out.println("Okay");
+
         try{
           Thread.sleep(20000);
-        }catch (InterruptedException ex) { System.out.println(ex); }
+        }catch (InterruptedException ex) {
+          System.out.println(ex);
+        }
       }
-
     }
 }
